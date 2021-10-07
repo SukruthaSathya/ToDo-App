4c41240a757c252a558da77bcb432f7fc1dfc6fb
Sukrutha <sukruthasathya@gmail.com>
2021-10-07 14:13:10 +0530

    App div divided into top and bottom divs

--- src/App.js
+++ src/App.js
@@ -6,48 +6,54 @@ function App() {
   const [todo, setTodo] = useState('')
   return (
     <div className="app">
-      <div className="mainHeading">
-        <h1>ToDo List</h1>
+      <div className="topDiv">
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="subHeading">
+          <br />
+          <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        </div>
+        <div className="input">
+          <div className="inputs">
+            <input value={todo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+            <i onClick={() => setTodos([...todos, { id: Date.now(), text: todo, status: false }])} className="fas fa-plus"></i>
+          </div>
+        </div>
       </div>
-      <div className="subHeading">
-        <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
-      </div>
-      <div className="input">
-        <input value={todo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setTodos([...todos, {id:Date.now(), text:todo,status:false}])} className="fas fa-plus"></i>
-      </div>
-      <div className="todos">
-        {todos.map((obj) => {
+      <div className="bottomDiv">
+        <div className="todos">
+          {todos.map((obj) => {
 
-          return (
-            <div className="todo">
-              <div className="left">
-                <input onChange={(e)=>{
-                  console.log(e.target.checked);
-                  console.log(obj);
-                  setTodos(todos.filter(obj2=>{
-                    if(obj2.id===obj.id){
-                      obj2.status=e.target.checked
-                    }
-                    return obj2
-                  }))
-                }} value={obj.status} type="checkbox" name="" id="" />
-                <p>{obj.text}</p>
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input onChange={(e) => {
+                    console.log(e.target.checked);
+                    console.log(obj);
+                    setTodos(todos.filter(obj2 => {
+                      if (obj2.id === obj.id) {
+                        obj2.status = e.target.checked
+                      }
+                      return obj2
+                    }))
+                  }} value={obj.status} type="checkbox" name="" id="" />
+                  <p>{obj.text}</p>
+                </div>
+                <div className="right">
+                  <i className="fas fa-times"></i>
+                </div>
               </div>
-              <div className="right">
-                <i className="fas fa-times"></i>
-              </div>
-            </div>
-          )
-        })}
+            )
+          })}
 
-        {todos.map((obj)=>{
-          if(obj.status){
-            return(<h1>{obj.text}</h1>)
-          }
-          return null
-        })}
+          {todos.map((obj) => {
+            if (obj.status) {
+              return (<h1>{obj.text}</h1>)
+            }
+            return null
+          })}
+        </div>
       </div>
     </div>
   );
