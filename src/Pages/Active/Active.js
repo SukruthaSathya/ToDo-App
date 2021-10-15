91fdd56c1b700be06876b1faaf3e3f5b7c0ba555
Sukrutha <sukruthasathya@gmail.com>
2021-10-16 01:08:12 +0530

    active design

--- src/Pages/Active/Active.js
+++ src/Pages/Active/Active.js
@@ -1,31 +1,46 @@
-import React,{useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
 import '../Home/Home.css'
-import {useHistory} from 'react-router'
+import { useHistory } from 'react-router'
 import { AppContext } from '../../context';
 
 
 function Active() {
-    const history=useHistory()
-    const handlePage=(e)=>{
+    const history = useHistory()
+    const handlePage = (e) => {
         e.preventDefault()
-        return(
-        history.push('/'))
+        return (
+            history.push('/'))
     }
-    const {todos}=useContext(AppContext)
-    
-    useEffect(()=>{
+    const { todos } = useContext(AppContext)
+
+    useEffect(() => {
         console.log(todos);
-    },[todos])
-    
+    }, [todos])
+
 
     return (
-        <div>
-        <div className='top'>
-            <span className="back" onClick={handlePage}> Home</span>
-        </div>
-        <div className="activeTodoList">
-            <h1>Text here{todos.text}</h1>
-        </div>
+        <div className='activeBody'>
+            <div className='top'>
+                <span className="back" onClick={handlePage}> Home</span>
+            </div>
+            <div className="head">
+               <h1>Active ToDos</h1>
+            </div>
+            <div className="input">
+                <div className="inputs">
+                    <div className="left">
+                        <input type="checkbox" />
+                    </div>
+                    <div className="listText">
+                        <h3>Hello</h3>
+                    </div>
+                    <div className="right">
+                    <i className="fas fa-times"></i>
+                    </div>
+                </div>
+            </div>
+                
+        
         </div>
     )
 }
